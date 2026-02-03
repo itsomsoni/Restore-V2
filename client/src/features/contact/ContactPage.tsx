2917ecdc3cdb6aa1b59f726a116f3fd54065c037
Om Soni <itsomsoni@gmail.com>
2026-02-03 16:21:37 +0530

    Redux Implementation

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,5 +1,29 @@
+// import { DecrementLegacy, IncrementLegacy, type CounterState } from "./counterReducer";
+import { Button, ButtonGroup, Typography } from "@mui/material";
+import { decrement, increment } from "./counterReducer";
+import { useAppDispatch, useAppSelector } from "../../app/store/store";
+
 export default function ContactPage() {
+
+  const { data } = useAppSelector(state => state.counter);
+  const dispatch = useAppDispatch();
+
   return (
-    <div>ContactPage</div>
+    <>
+      <Typography variant="h4" gutterBottom>
+        Contact Page
+      </Typography>
+      <Typography variant="body1">
+        The current counter value is: {data}
+      </Typography>
+      <ButtonGroup>
+        <Button color="error" onClick={() => dispatch(decrement(1))}>Decrement</Button>
+        <Button color="secondary" onClick={() => dispatch(increment(1))}>Increment</Button>
+        <Button color="primary" onClick={() => dispatch(increment(5))}>Increment by 5</Button>
+        {/* <Button color="error" onClick={() => dispatch(DecrementLegacy())}>Decrement</Button>
+        <Button color="secondary" onClick={() => dispatch(IncrementLegacy())}>Increment</Button>
+        <Button color="primary" onClick={() => dispatch(IncrementLegacy(5))}>Increment by 5</Button> */}
+      </ButtonGroup>
+    </>
   )
 }
---
+++ client/src/features/contact/counterReducer.ts
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type CounterState = {
+    data: number;
+}
+
+const initialState: CounterState = {
+    data: 42
+}
+
+export const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state, action) => {
+            state.data += action.payload;
+        },
+        decrement: (state, action) => {
+            state.data -= action.payload;
+        }
+    }
+});
+
+export const { increment, decrement } = counterSlice.actions;
+
+export function IncrementLegacy(amount: number = 1) {
+    return {
+        type: 'INCREMENT',
+        payload: amount
+    };
+}
+
+export function DecrementLegacy(amount: number = 1) {
+    return {
+        type: 'DECREMENT',
+        payload: amount
+    };
+}
+
+export default function counterReducer(state = initialState, action: { type: string, payload: number }): CounterState {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { ...state, data: state.data + action.payload };
+        case 'DECREMENT':
+            return { ...state, data: state.data - action.payload };
+        default:
+            return state;
+    }
+}
